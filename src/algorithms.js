8f547b82709da11ef6cc001eec0927a7e5ca58c0
Baek Jiyeon <100.paperkite@gmail.com>
2022-03-28 00:14:34 +0900

    feat: 버블 소트 시각화 기본 구현 완료.

---
+++ src/algorithms.js
@@ -0,0 +1,12 @@
+export function* BubbleSort(array) {
+  let steps = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array.nodes[j] > array.nodes[j + 1]) {
+        array.swap(j, j + 1);
+        yield ++steps;
+      }
+    }
+  }
+}
--- src/components/NodeArray.js
+++ src/components/NodeArray.js
@@ -1,8 +1,28 @@
 import Node from './Node.js';
 
 class NodeArray {
+  #selected = [];
+
   constructor(numbers) {
-    this.nodes = numbers.map((number) => new Node(number));
+    this.nodes = numbers.map((x) => new Node(x));
+  }
+
+  get length() {
+    return this.nodes.length;
+  }
+
+  select(indexes) {
+    this.#selected.forEach((index) => this.nodes[index].unselect());
+    this.#selected = indexes;
+    indexes.forEach((index) => this.nodes[index].select());
+  }
+
+  swap(i, j) {
+    this.select([i, j]);
+    // @todo animation
+    const temp = this.nodes[i];
+    this.nodes[i] = this.nodes[j];
+    this.nodes[j] = temp;
   }
 
   render() {
