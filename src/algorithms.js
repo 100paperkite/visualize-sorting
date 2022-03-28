39cb911bfbff874a90447058e754c58da6e48b0c
Baek Jiyeon <100.paperkite@gmail.com>
2022-03-28 21:44:56 +0900

    feat: Node State 추가하여 비교 상태 구현. 관련 효과 추가.

--- src/algorithms.js
+++ src/algorithms.js
@@ -1,12 +1,14 @@
 export function* BubbleSort(array) {
   let steps = 0;
 
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length - 1; j++) {
-      if (array.nodes[j] > array.nodes[j + 1]) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array.compare(j, j + 1, (i, j) => i > j)) {
+        yield;
         array.swap(j, j + 1);
       }
       yield ++steps;
     }
   }
+  return steps;
 }
--- src/components/Node.js
+++ src/components/Node.js
@@ -1,6 +1,9 @@
+import NodeState from './NodeState.js';
+
 class Node {
   constructor(value) {
     this.initElement(value);
+    this.state = NodeState.NONE;
   }
 
   initElement(value) {
@@ -9,13 +12,28 @@ class Node {
     this.$element.appendChild(document.createTextNode(value));
   }
 
-  select() {
-    this.$element.classList.add('selected');
+  set state(state) {
+    const namesOfStates = Object.keys(NodeState).map((state) => state.toLowerCase());
+    this.$element.classList.remove(...namesOfStates);
+    switch (state) {
+      case NodeState.NONE:
+        this.$element.classList.add('none');
+        break;
+      case NodeState.COMPARED:
+        this.$element.classList.add('compared');
+        break;
+      case NodeState.SWAPPED:
+        this.$element.classList.add('swapped');
+        break;
+      default:
+        console.log(`${state} is not a valid state.`);
+    }
   }
 
-  unselect() {
-    this.$element.classList.remove('selected');
+  get state() {
+    return this.state;
   }
+
   // for comparison operator
   valueOf() {
     return parseFloat(this.$element.textContent);
